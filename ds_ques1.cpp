e831d527ef0283fd1ad8ae2c7210bf3bb2f5a6e9
adityaharsh2001 <geekyadi118@gmail.com>
2022-10-22 22:06:22 +0530

    ques 9 added

---
+++ ds_ques1.cpp
@@ -0,0 +1,31 @@
+/*
+20. Given a full m-ary tree with i internal vertices, Write a Program to find the number of leaf
+nodes.
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int m;//value of m in m-ary
+	int i;//number of internal vertices
+	cout<<"Enter the value of m in a m-ary tree"<<endl;
+	cin>>m;
+	cout<<"Enter the number of internal vertices"<<endl;
+	cin>>i;
+	int l;
+	l=(m-1)*i+1;//formula for finding the number of leaf nodes if internal vertices for full m-ary tree is given.
+	cout<<"The number of leaf nodes are "<<l<<endl;
+	return 0;
+}
+
+//OUTPUt
+/*
+Enter the value of m in a m-ary tree
+4
+Enter the number of internal vertices
+33
+The number of leaf nodes are 100
+
+C:\Users\Lenovo-Laptop\Desktop\Backup\Second semester\discrete structures>
+*/
